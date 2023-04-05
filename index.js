c5463ddb9b3f7b8479797abeafeaa4cda4c824d5
HinataYasukawa <h1i2n2a62208@gmail.com>
2023-04-05 14:36:18 +0900

    make form

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    add();
+})
+
+function add(){
+    let todoText = input.value;
+    if(todoText.length > 0){
+        const li = document.createElement("li");
+        li.innerText = todoText;
+        li.classList.add("list-group-item");
+        ul.appendChild(li);
+        input.value = "";
+    }
+}
