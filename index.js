9c89edcdd2a638bdff9328cd8340a4c638081630
HinataYasukawa <h1i2n2a62208@gmail.com>
2023-04-05 17:16:22 +0900

    make Description

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
+const button = button.getElementById("button");
 
 const todos = JSON.parse(localStorage.getItem("todos"));
 
+//add edit button
+if(li){
+button.addEventLitener("click", function(event){
+    ul.addEventLitner("click", function(event){
 
+})
+})
+}
+
+//Show todo list
 if(todos){
     todos.forEach(todo => {
         add(todo);
     })
 }
 
+//prevent page updates
 form.addEventListener("submit", function(event){
     event.preventDefault();
     add();
 })
-
 
 function add(todo){
     let todoText = input.value;
@@ -24,6 +34,7 @@ if(todo){
     todoText = todo.text;
 }
 
+    //make list
     if(todoText){
         const li = document.createElement("li");
         li.innerText = todoText;
@@ -33,12 +44,14 @@ if(todo){
             li.classList.add("text-decoration-line-through");
         }
 
+        //remove list
         li.addEventListener("contextmenu", function(event){
             event.preventDefault();
             li.remove();
             saveData();
         })
 
+        //cross out
         li.addEventListener("click", function(){
             li.classList.toggle("text-decoration-line-through");
             saveData();
@@ -50,10 +63,12 @@ if(todo){
     }
 }
 
+//make savedata
 function saveData(){
     const lists = document.querySelectorAll("li");
     let todos = [];
 
+    //make 2 elements
     lists.forEach(list => {
         let todo = {
             text: list.innerText,
@@ -61,5 +76,7 @@ function saveData(){
         };
         todos.push(todo);
     })
+
+    //save data
     localStorage.setItem("todos", JSON.stringify(todos));
 }
